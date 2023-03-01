7c8648fb34b37e61474119c346cb620e794518a1
Charles Bruno <cbsilva26@gmail.com>
2023-03-01 01:04:03 -0400

    first commit

---
+++ beneficiario/js/mask.js
@@ -0,0 +1,76 @@
+//MASCARA DE TELEFONE
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
+
+  //MASCARA DE CPF
+  function is_cpf (c) {
+
+    if((c = c.replace(/[^\d]/g,"")).length != 11)
+      return false
+  
+    if (c == "00000000000")
+      return false;
+  
+    var r;
+    var s = 0;
+  
+    for (i=1; i<=9; i++)
+      s = s + parseInt(c[i-1]) * (11 - i);
+  
+    r = (s * 10) % 11;
+  
+    if ((r == 10) || (r == 11))
+      r = 0;
+  
+    if (r != parseInt(c[9]))
+      return false;
+  
+    s = 0;
+  
+    for (i = 1; i <= 10; i++)
+      s = s + parseInt(c[i-1]) * (12 - i);
+  
+    r = (s * 10) % 11;
+  
+    if ((r == 10) || (r == 11))
+      r = 0;
+  
+    if (r != parseInt(c[10]))
+      return false;
+  
+    return true;
+  }
+  
+  
+  function fMasc(objeto,mascara) {
+  obj=objeto
+  masc=mascara
+  setTimeout("fMascEx()",1)
+  }
+  
+    function fMascEx() {
+  obj.value=masc(obj.value)
+  }
+  
+  function mCPF(cpf){
+  cpf=cpf.replace(/\D/g,"")
+  cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+  return cpf
+  }
+  
+  cpfCheck = function (el) {
+      document.getElementById('cpfResponse').innerHTML = is_cpf(el.value)? '<span style="color:green">válido</span>' : '<span style="color:red">inválido</span>';
+      if(el.value=='') document.getElementById('cpfResponse').innerHTML = '';
+  }
